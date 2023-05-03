0cb26aaa4792ef1a35bb0ed18502f8c7902aea63
MemphiS047 <hakverdiyigit@gmail.com>
2023-05-03 19:32:16 +0200

    Updated sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { sidebar } from "/src/css/sidebar.module.css"
+import { sidebar, avatarContainer, links, name, role } from "/src/css/sidebar.module.css"
 
 const Sidebar = () => {
     return (
         <div className={sidebar}>
-            <ul>
-                <li>
-                    <Link to="/">
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/about">
-                        About
-                    </Link>
-                </li>
-            </ul>
+            <div>
+                <div className={avatarContainer}>
+                    <img src="https://via.placeholder.com/200x200.png?text=Avatar" alt="Avatar" className="avatar" />
+
+                </div>
+                <div>
+                    <div className={name}>Yigit Hakverdi</div>
+                    <div className={role}>Security Researcher</div>
+                </div>
+            </div>
+            <div>
+                <ul className={links}>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About Me</Link></li>
+                </ul>
+            </div>
         </div>
     );
 }
